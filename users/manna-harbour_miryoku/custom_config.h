cc65bca496c32fde5e81626e95c2a8de5c286304
Jakob Hansen <jakobhansen@microsoft.com>
2024-12-27 21:06:49 +0100

    Initial setup in Miryoku userspace

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,3 +5,44 @@
 
 #pragma once
 
+#define XXX KC_NO
+
+#define MIRYOKU_LAYERMAPPING_BASE( \
+      K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
+      K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
+      K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29, \
+      N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
+) \
+LAYOUT_split_3x6_3( \
+QK_CAPS_WORD_TOGGLE,  KC_Q,  KC_W, KC_F,  KC_P,  KC_B,         KC_Y,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_LBRC, \
+OSM(MOD_LSFT),  LCTL_T(KC_A),  LALT_T(KC_R),  LGUI_T(KC_S),  LSFT_T(KC_T),  KC_G,  KC_H,  LSFT_T(KC_N),  LGUI_T(KC_E),  LALT_T(KC_O),  LCTL_T(KC_I),  KC_QUOT, \
+XXX,  KC_Z,  KC_X,  KC_C,  KC_D,  KC_V,         KC_M,  KC_U,  KC_COMMA,  KC_DOT,  KC_MINUS,  XXX, \
+LT(U_MOUSE,KC_ESC),  LT(U_NAV,KC_SPC),  LT(U_MEDIA,KC_TAB),  LT(U_NUM,KC_BSPC),  LT(U_SYM,KC_ENT),  K37 \
+)
+
+
+#define MIRYOKU_LAYERMAPPING_SYM( \
+      K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
+      K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
+      K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29, \
+      N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
+) \
+LAYOUT_split_3x6_3( \
+XXX,  KC_EXLM,  KC_QUES,  KC_AT,  KC_COLN,  KC_ASTR, K05,  K06,  K07,  K08,  K09,  XXX, \
+KC_AMPR,  KC_QUOT,  KC_DQUO,  KC_LPRN,  KC_RPRN,  KC_EQL,         K15,  K16,  K17,  K18,  K19,  XXX, \
+KC_DLR,  KC_HASH,  KC_PERC,  KC_LCBR,  KC_RCBR,  KC_PIPE,         K25,  K26,  K27,  K28,  K29,  XXX , \
+                  KC_LBRC,  KC_RBRC,  KC_SLSH,         K35,  K36,  K37 \
+)
+
+#define MIRYOKU_LAYERMAPPING_NUM( \
+      K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
+      K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
+      K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29, \
+      N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
+) \
+LAYOUT_split_3x6_3( \
+XXX,  KC_TILD,  K01,  K02,  K03,  KC_CIRC,         K05,  K06,  K07,  K08,  K09,  XXX, \
+XXX,  KC_GRV,  K11,  K12,  K13,  KC_EQL,         K15,  KC_LSFT,  KC_LGUI,  KC_LALT,  KC_LCTL,  XXX, \
+XXX,  KC_0,  K21,  K22,  K23,  KC_PLUS,         K25,  K26,  K27,  K28,  K29,  XXX, \
+                  KC_LBRC,  KC_RBRC,  KC_BSLS,         K35,  K36,  K37 \
+)
--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,9 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+AUTO_SHIFT_ENABLE = no
+
+# r2g rules
+OLED_ENABLE     = no
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
