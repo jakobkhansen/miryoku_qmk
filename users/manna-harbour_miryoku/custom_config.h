e494e77eb3de76bcf3d2934d32fa2a00cb1dcd4d
Jakob Hansen <jakobhansen@microsoft.com>
2025-01-06 16:07:11 +0100

    Add some options

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -3,6 +3,13 @@
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+#undef TAPPING_TERM
+#define TAPPING_TERM 165
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
 #pragma once
 
 #define XXX KC_NO
