7d73423a4632b0ca719f36717851b2db194da8ca
Jakob Hansen <jakobhansen@microsoft.com>
2025-01-13 16:19:26 +0100

    combo one shot shift and double tap for caps word

--- users/manna-harbour_miryoku/custom_code.c
+++ users/manna-harbour_miryoku/custom_code.c
@@ -4,11 +4,10 @@
 
 #include "manna-harbour_miryoku.h"
 
-const uint16_t PROGMEM caps_word[] = {RSFT_T(KC_N), LGUI_T(KC_E), COMBO_END};
-const uint16_t PROGMEM esc_combo[] = {LGUI_T(KC_S), LSFT_T(KC_T), COMBO_END};
+const uint16_t PROGMEM shift_combo[] = {RSFT_T(KC_N), LGUI_T(KC_E), COMBO_END};
+const uint16_t PROGMEM esc_combo[]   = {LGUI_T(KC_S), LSFT_T(KC_T), COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(esc_combo, KC_ESC),
-    COMBO(caps_word, CW_TOGG),
+    COMBO(shift_combo, OSM(MOD_LSFT)),
 };
-
