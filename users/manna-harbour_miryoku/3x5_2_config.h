093b4a21cee871c9ecef83f2463cd6ff17a99fbe
Jakob Hansen <jakobhansen@microsoft.com>
2025-04-03 22:00:00 +0200

    Move layers a bit

--- users/manna-harbour_miryoku/3x5_2_config.h
+++ users/manna-harbour_miryoku/3x5_2_config.h
@@ -9,9 +9,9 @@
       N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
 ) \
 LAYOUT_split_3x5_2( \
-  KC_Q,          KC_W,          KC_F,          KC_P,          KC_B,               KC_Y,  KC_J,          KC_K,          KC_L,          KC_SCLN,        \
-  LCTL_T(KC_A),  LALT_T(KC_R),  LGUI_T(KC_S),  LSFT_T(KC_T),  LT(U_FUN, KC_G),    KC_H,  RSFT_T(KC_N),  LGUI_T(KC_E),  LALT_T(KC_O),  LCTL_T(KC_I),   \
-  KC_Z,          KC_X,          KC_C,          KC_D,          KC_V,               KC_M,  KC_U,          KC_COMMA,      KC_DOT,        KC_MINUS,       \
+  KC_Q,            KC_W,         KC_F,          KC_P,          KC_B,               KC_Y,  KC_J,          KC_K,          KC_L,          KC_SCLN,        \
+  LCTL_T(KC_A),    LALT_T(KC_R), LGUI_T(KC_S),  LSFT_T(KC_T),  LT(U_FUN, KC_G),    KC_H,  RSFT_T(KC_N),  LGUI_T(KC_E),  LALT_T(KC_O),  LCTL_T(KC_I),   \
+  LT(U_FUN, KC_Z), KC_X,         KC_C,          KC_D,          KC_V,               KC_M,  KC_U,          KC_COMMA,      KC_DOT,        KC_MINUS,       \
                                 LSFT_T(KC_TAB),   LT(U_NAV,KC_SPC),               LT(U_NUM,KC_BSPC),    LT(U_SYM,KC_ENT)  \
 )
 
@@ -48,10 +48,10 @@ LAYOUT_split_3x5_2( \
       N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39  \
 ) \
 LAYOUT_split_3x5_2( \
-  QK_BOOT,        KC_F7,          KC_F8,          KC_F9,         KC_F12,    K05,     KC_DOWN,  KC_UP,    KC_RIGHT,  K09,   \
-  LCTL_T(K10),    LALT_T(KC_F4),  LGUI_T(KC_F5),  LSFT_T(KC_F6), KC_F11,    KC_LEFT, KC_VOLD,  KC_VOLU,  K18,       K19,   \
-  K20,            KC_F1,          KC_F2,          KC_F3,         KC_F10,    K25,     KC_BRID,  KC_BRIU,  K28,       K29,   \
-                                                  K33,           K34,       K35,     K36                                   \
+  XXX,     LGUI(KC_7),         LGUI(KC_8),         LGUI(KC_9),         XXX,  K05,     KC_DOWN,  KC_UP,   KC_RIGHT, K09,   \
+  KC_LCTL, LALT_T(LGUI(KC_4)), LGUI_T(LGUI(KC_5)), LSFT_T(LGUI(KC_6)), XXX,  KC_LEFT, KC_VOLD,  KC_VOLU, K18,      K19,   \
+  XXX,     LGUI(KC_1),         LGUI(KC_2),         LGUI(KC_3),         XXX,  K25,     KC_BRID,  KC_BRIU, K28,      K29,   \
+                                                   K33,                K34,  K35,     K36         \
 )
 
 // Special layer which allows pressing cmd+num with one hand (switch workspace)
@@ -62,8 +62,8 @@ LAYOUT_split_3x5_2( \
       N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39  \
 ) \
 LAYOUT_split_3x5_2( \
-  XXX, LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), XXX,   XXX, XXX, XXX, XXX, XXX,   \
-  XXX, LGUI(KC_4), LGUI(KC_5), LGUI(KC_6), XXX,   XXX, XXX, XXX, XXX, XXX,   \
-  XXX, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), XXX,   XXX, XXX, XXX, XXX, XXX,   \
-                               KC_LSFT,    XXX,   XXX, XXX                   \
+  QK_BOOT,        KC_F7,          KC_F8,          KC_F9,         KC_F12,   XXX, XXX, XXX, XXX, XXX,   \
+  LCTL_T(K10),    LALT_T(KC_F4),  LGUI_T(KC_F5),  LSFT_T(KC_F6), KC_F11,   XXX, XXX, XXX, XXX, XXX,   \
+  K20,            KC_F1,          KC_F2,          KC_F3,         KC_F10,   XXX, XXX, XXX, XXX, XXX,   \
+                                                  KC_LSFT,       XXX,      XXX, XXX                   \
 )
--- users/manna-harbour_miryoku/custom_code.c
+++ users/manna-harbour_miryoku/custom_code.c
@@ -4,10 +4,34 @@
 
 #include "manna-harbour_miryoku.h"
 
-const uint16_t PROGMEM pipe_combo[] = {RSFT_T(KC_N), LGUI_T(KC_E), COMBO_END};
+const uint16_t PROGMEM pipe_combo[] = {KC_COMMA, KC_DOT, COMBO_END};
 const uint16_t PROGMEM esc_combo[]  = {LGUI_T(KC_S), LSFT_T(KC_T), COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(esc_combo, KC_ESC),
     COMBO(pipe_combo, KC_PIPE),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LALT_T(LGUI(KC_4)):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_4));
+                return false;
+            }
+            break;
+        case LGUI_T(LGUI(KC_5)):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_5));
+                return false;
+            }
+            break;
+        case LSFT_T(LGUI(KC_6)):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_6));
+                return false;
+            }
+            break;
+    }
+    return true;
+}
