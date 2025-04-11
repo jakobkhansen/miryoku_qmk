5a61c2d276da6719ab53b9b0db5d6b77d29bff0c
Jakob Hansen <jakob.hansen@hotmail.no>
2025-04-11 21:48:59 +0200

    Cleanup

--- users/manna-harbour_miryoku/custom_code.c
+++ users/manna-harbour_miryoku/custom_code.c
@@ -37,27 +37,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 #ifdef POINTING_DEVICE_ENABLE
-#    include "pointing_device.h"
-#    include "pointing_device_gestures.h"
+#include "pointing_device.h"
+#include "pointing_device_gestures.h"
 
 // Move accumulation when in deadzone
-float move_accumulated_x = 0;
-float move_accumulated_y = 0;
+int8_t move_accumulated_x = 0;
+int8_t move_accumulated_y = 0;
 
 // Modify these values to adjust the scrolling speed
-#    define SCROLL_DIVISOR_H 20.0
-#    define SCROLL_DIVISOR_V 20.0
+#define SCROLL_DIVISOR_H 20.0
+#define SCROLL_DIVISOR_V 20.0
 
 // Variables to store accumulated scroll values
 float scroll_accumulated_h = 0;
 float scroll_accumulated_v = 0;
 
 // Cursor movement
-#    define DEADZONE 2 // Set this to the number of counts to ignore
+#define DEADZONE 2 // Set this to the number of counts to ignore
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     pointing_device_task_auto_mouse(mouse_report);
     if (layer_state_is(U_NAV) || layer_state_is(U_NUM)) {
+        if (mouse_report.x == 0 && mouse_report.y == 0) {
+            return mouse_report;
+        }
         cirque_pinnacle_enable_cursor_glide(true);
         // Calculate and accumulate scroll values based on mouse movement and divisors
         scroll_accumulated_h += (float)mouse_report.x / SCROLL_DIVISOR_H;
@@ -78,11 +81,11 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     }
     cirque_pinnacle_enable_cursor_glide(false);
     // Accumulate movement
-    move_accumulated_x += (float)mouse_report.x;
-    move_accumulated_y += (float)mouse_report.y;
+    move_accumulated_x += mouse_report.x;
+    move_accumulated_y += mouse_report.y;
 
-    int8_t move_x = (int8_t)move_accumulated_x;
-    int8_t move_y = (int8_t)move_accumulated_y;
+    int8_t move_x = move_accumulated_x;
+    int8_t move_y = move_accumulated_y;
 
     move_accumulated_x -= move_x;
     move_accumulated_y -= move_y;
@@ -99,8 +102,6 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 
     mouse_report.x = move_x;
     mouse_report.y = move_y;
-
-    return mouse_report;
 
     return mouse_report;
 }
